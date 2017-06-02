5fa182c6ab192702b1c3a419eea80c9f76781f5f
vgrys <vgrysster@gmail.com>
2017-06-02 16:57:28 +0300

    Update Cell.java

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -14,7 +14,7 @@ public enum Cell {
     private String symbol;
 
     private Cell(final String initialSymbol) {
-        this.symbol = initialSymbol;
+        this.symbol = initialSymbol
     }
 
 	// Override function for easy printing of cell's symbol
