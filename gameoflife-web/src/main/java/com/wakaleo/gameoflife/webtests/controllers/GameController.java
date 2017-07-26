4387729db74e70b731e4d26051e7d5898632dd4f
vgrys <vgrysster@gmail.com>
2017-07-26 05:01:00 +0300

    fixed issue in GameController.java

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -20,12 +20,8 @@ import com.wakaleo.gameoflife.domain.Universe;
  */
 @Controller
 @RequestMapping("/game")
-<<<<<<< HEAD
 
 // test new GIT hook
-
-=======
->>>>>>> origin
 public class GameController {
 	// For generating random thread sleep times
     private Random randomGenerator = new Random();
